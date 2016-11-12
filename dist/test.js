a929e1a3106814d1901f095c4b7cde561bbd41d2
Dustin L. Dodson <dustinleedodson@gmail.com>
2016-11-11 18:57:05 -0900

    Turned everything into an inheritance-type model.

--- dist/test.js
+++ dist/test.js
@@ -1,23 +1,9 @@
 //NODE
+var Page = require('../lib/Page.js'),
+    Glyphicon = require('../lib/Glyphicon.js');
 
-var bs = require('./bootstrap.js');
-var glyph = new bs.Glyphicon('send');
-var glyph2 = new bs.Glyphicon('star-empty', true);
-var glyph3 = new bs.Glyphicon('send');
-var glyph4 = new bs.Glyphicon('star-empty', true);
-
-var page = new bs.Page();
-
-var col1 = new bs.Column(3);
-var col2 = new bs.Column(3);
-var col3 = new bs.Column(3);
-var col4 = new bs.Column(3);
-col1.addChild(glyph);
-col2.addChild(glyph2);
-col3.addChild(glyph3);
-col4.addChild(glyph4);
-
-var row = new bs.Row([col1, col2, col3, col4]);
-page.addChild(row);
+var page = new Page();
+var glyph = new Glyphicon('euro');
+page.addChild(glyph);
 
 console.log(page.toString());
--- gulpfile.js
+++ gulpfile.js
@@ -6,14 +6,19 @@ var gulp   = require('gulp'),
 	insert = require('gulp-insert');
 
 gulp.task('script-min', function() {
-	gulp.src('lib/**/*.js')
+	gulp.src('[lib/**/bootstrapcomponent.js', 'lib/**/*.js'])
 	.pipe(jshint())
 	.pipe(insert.prepend('bs.'))
 	.pipe(concat('bootstrap.js'))
-	.pipe(insert.transform(function(contents, file) {
-		contents.replace(/\n?\r/g, "\n\t");
-		return contents;
-	}))
+	/*.pipe(insert.transform(function(contents, file) {
+		var ret = '';
+		var splitContents = contents.split('\n');
+
+		for(var i = 0; i < splitContents.length; i++)
+			ret += '\t' + splitContents[i] + '\n';
+
+		return ret;
+	}))*/
 	.pipe(insert.wrap('bs={};\n','\nmodule.exports=bs;'))
 	// .pipe(uglify())
 	// .pipe(rename({suffix:'.min'}))
