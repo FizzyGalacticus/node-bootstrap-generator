449b1f638de65559300c7fadffee7773f56c24e0
Dustin L. Dodson <dustinleedodson@gmail.com>
2016-11-11 18:00:26 -0900

    Added page, row, and column objects.

--- dist/test.js
+++ dist/test.js
@@ -2,8 +2,22 @@
 
 var bs = require('./bootstrap.js');
 var glyph = new bs.Glyphicon('send');
-var glyph2 = new bs.Glyphicon('send', true);
-glyph.addChild(glyph2);
-var str = glyph.toString();
+var glyph2 = new bs.Glyphicon('star-empty', true);
+var glyph3 = new bs.Glyphicon('send');
+var glyph4 = new bs.Glyphicon('star-empty', true);
 
-console.log(glyph.toString());
+var page = new bs.Page();
+
+var col1 = new bs.Column(3);
+var col2 = new bs.Column(3);
+var col3 = new bs.Column(3);
+var col4 = new bs.Column(3);
+col1.addChild(glyph);
+col2.addChild(glyph2);
+col3.addChild(glyph3);
+col4.addChild(glyph4);
+
+var row = new bs.Row([col1, col2, col3, col4]);
+page.addChild(row);
+
+console.log(page.toString());
